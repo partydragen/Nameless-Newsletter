d68046d97ba38d7863145780988dffdf8ec8c995
Partydragen <admin@partydragen.com>
2022-12-02 16:57:10 +0100

    Release v1.0.1

--- upload/modules/Newsletter/module.php
+++ upload/modules/Newsletter/module.php
@@ -20,7 +20,7 @@ class Newsletter_Module extends Module {
 
         $name = 'Newsletter';
         $author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a>';
-        $module_version = '1.0.0';
+        $module_version = '1.0.1';
         $nameless_version = '2.0.2';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
